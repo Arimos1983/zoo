cafd31b312fb72ed4355cbb67caba1bcf104bb6e
Arimos <amilinkovic.ns@gmail.com>
2018-08-07 19:08:01 +0200

    zadatak 3

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,10 +1,13 @@
 <template>
   <div >
-      <tr v-for="animal in animals" :key="animal.name">
+      <tr v-for="animal in animals" :key="animal.id">
           <td>{{animal.name}}</td>
           <td>{{animal.species}}</td>
           <td v-if="animal.yearOfBirth != '' ">{{animal.yearOfBirth}}</td>
           <td v-else>{{'Unknown'}}</td>
+          <td>
+              <button @click="removeAnimal(animal)">Remove</button>
+          </td>
       </tr>
   </div>
 </template>
@@ -18,14 +21,23 @@ export default {
         return {
 
             animals: [
-                {name: "Pera", species: "Lav", yearOfBirth: "2001" },
-                {name: "Mile", species: "Delfin", yearOfBirth: "2007" },
-                {name: "Milica", species: "Zebra", yearOfBirth: "2011" },
-                {name: "Jovan", species: "Soko", yearOfBirth: "" },
-                {name: "Snesko", species: "Polarni medved", yearOfBirth: "1998" },
+                {id: 1, name: "Pera", species: "Lav", yearOfBirth: "2001" },
+                {id: 2,name: "Mile", species: "Delfin", yearOfBirth: "2007" },
+                {id: 3,name: "Milica", species: "Zebra", yearOfBirth: "2011" },
+                {id: 4,name: "Jovan", species: "Soko", yearOfBirth: "" },
+                {id: 5,name: "Snesko", species: "Polarni medved", yearOfBirth: "1998" },
             ]
 
         };
+    },
+
+    methods: 
+    {
+        removeAnimal(animal)
+        {
+            let animalToDelete = this.animals.indexOf(animal);
+            this.animals.splice(animalToDelete,1);
+        }
     }
   
 }
