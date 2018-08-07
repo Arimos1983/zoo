d2c95f4fb422e367e8b962e9131e76052e0bd77a
Arimos <amilinkovic.ns@gmail.com>
2018-08-07 18:48:32 +0200

    zadatak 1

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div >
+      <tr v-for="animal in animals" :key="animal.name">
+          <td>{{animal.name}}</td>
+          <td>{{animal.species}}</td>
+          <td>{{animal.yearOfBirth}}</td>
+      </tr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalList',
+
+    data()
+    {
+        return {
+
+            animals: [
+                {name: "Pera", species: "Lav", yearOfBirth: "2001" },
+                {name: "Mile", species: "Delfin", yearOfBirth: "2007" },
+                {name: "Milica", species: "Zebra", yearOfBirth: "2011" },
+                {name: "Jovan", species: "Soko", yearOfBirth: "2015" },
+                {name: "Snesko", species: "Polarni medved", yearOfBirth: "1998" },
+            ]
+
+        };
+    }
+  
+}
+</script>
+
