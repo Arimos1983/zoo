74f90df1c526dbf3a7d68591bda9d353f93f818f
Arimos <amilinkovic.ns@gmail.com>
2018-08-07 20:29:01 +0200

    zadatak 5

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,17 +1,25 @@
 <template>
   <div >
-      <tr v-for="animal in animals" :key="animal.id">
-          <td>{{animal.name}}</td>
-          <td>{{animal.species}}</td>
-          <td v-if="animal.yearOfBirth != '' ">{{animal.yearOfBirth}}</td>
-          <td v-else>{{'Unknown'}}</td>
-          <td>
-              <button @click="removeAnimal(animal)">Remove</button>
-          </td>
-          <td>
-              <button @click="moveToTop(animal)">Move up</button>
-          </td>
-      </tr>
+      <form @submit.prevent >
+          <input v-model="newAnimals.name" type="text" placeholder="name" > <br>
+          <input v-model="newAnimals.species" type="text" placeholder="species"> <br>
+          <input v-model="newAnimals.yearOfBirth" type="text" placeholder="year of birth"> <br>
+          <button @click="addAnimal" type="submit">Add animal</button>
+      </form>
+      <table>
+        <tr v-for="animal in animals" :key="animal.id">
+            <td>{{animal.name}}</td>
+            <td>{{animal.species}}</td>
+            <td v-if="animal.yearOfBirth != '' ">{{animal.yearOfBirth}}</td>
+            <td v-else>{{'Unknown'}}</td>
+            <td>
+                <button @click="removeAnimal(animal)">Remove</button>
+            </td>
+            <td>
+                <button @click="moveToTop(animal)">Move up</button>
+            </td>
+        </tr>
+      </table>
   </div>
 </template>
 
@@ -22,9 +30,9 @@ export default {
     data()
     {
         return {
-
+            newAnimals: {},
             animals: [
-                {id: 1, name: "Pera", species: "Lav", yearOfBirth: "2001" },
+                {id: 1,name: "Pera", species: "Lav", yearOfBirth: "2001" },
                 {id: 2,name: "Mile", species: "Delfin", yearOfBirth: "2007" },
                 {id: 3,name: "Milica", species: "Zebra", yearOfBirth: "2011" },
                 {id: 4,name: "Jovan", species: "Soko", yearOfBirth: "" },
@@ -46,6 +54,11 @@ export default {
             let animalToMove = this.animals.indexOf(animal);
             this.animals.splice(animalToMove,1);
             this.animals.unshift(animal);
+        },
+        addAnimal()
+        {
+            this.animals.push(this.newAnimals);
+            this.newAnimals = {};
         }
     }
   
